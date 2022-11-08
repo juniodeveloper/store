ba5b2fe9025c09092c60c5bc2ec7126e621b6103
juniodeveloper <junio@juniodev.com>
2022-11-08 15:59:45 -0300

    Removed index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import LayoutStore from '../src/layouts/LayoutStore'
-import styles from '../styles/pages/Home.module.css'
 
 export default function Home () {
   return (
     <LayoutStore title='MyStore' description='Baixe os melhores aplicativos totalmente grátis.'>
-      <div className={styles.home}>
+      <div>
 
       </div>
     </LayoutStore>
